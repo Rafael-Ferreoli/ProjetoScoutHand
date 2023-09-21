49e7c900ee4d706a43c9855ec826d999976d6687
Rafael Ferreira <rafaelferreoli@gmail.com>
2023-09-21 14:18:55 -0300

    Versão 1.4 TCC

---
+++ src/main/java/com/mycompany/handballscoutapp/HandballScoutApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.handballscoutapp;
+
+/**
+ *
+ * @author ifmaker
+ */
+
+public class HandballScoutApp {
+    public static void main(String[] args) {
+        FrQuadra tela1 = new FrQuadra();
+        tela1.setVisible(true);
+}
+}
